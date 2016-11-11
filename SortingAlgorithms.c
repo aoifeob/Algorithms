a672fa6b58f2795347d9abaa5596a6c08958c837
obriea21 <obriea21@tcd.ie>
2016-11-11 16:47:24 +0000

    Update SortingAlgorithms.c

--- SortingAlgorithms.c
+++ SortingAlgorithms.c
@@ -9,7 +9,6 @@ int main(){
 
 //declare variables
 int size; //size of array (ie the number of things the array holds)
-int ages[8]; // array of test data to be sorted
 int i; //counter
 int ages_InsSort[8]; // array sorted by insertion sort
 int ages_Quicksort[8]; // array sorted by quicksort
@@ -43,10 +42,89 @@ return 0;
 
 
 // 1. Insertion Sort function
-int InsSort(){
+int InsSort(int ages[8], int ages_InsSort[8], int i){
+
+//Step 1: Insert ages[0] into sorting list.  As list is only one item long, consider it sorted.
+//Step 2: Insert ages[1] into sorting list.  Compare elements [1] and [0].  If [0]<[1], do nothing.  If [0]>[1], swap the elements.
+//Step 3: Insert ages[2] into sorting list.  Compare elements [2] and [1].  If [1]<[2], do nothing.
+
+ages_InsSort[0] = ages[0];
+
+//Now consider ages[1].  Compare with ages[0].
+if (ages[1]<ages[0]){
+ages_InsSort[0] = ages[1];
+ages_InsSort[1] = ages[0];
+}
+else{
+    ages_InsSort[1] = ages[1];
+}
+
+//Now consider ages[2].
+if (ages[2]<ages_InsSort[1]){
+        ages_InsSort[2] = ages_InsSort[1];
+    if (ages[2]<ages_InsSort[0]){
+        ages_InsSort[1] = ages_InsSort[0];
+        ages_InsSort[0] = ages[2];
+    }
+    else{
+        ages_InsSort[1] = ages[2];
+    }
+}
+else{
+    ages_InsSort[2] = ages[2];
+}
+
+//Now consider ages[3].
+if (ages[3]<ages_InsSort[2]){
+    ages_InsSort[3] = ages_InsSort[2];
+    if (ages[3]<ages_InsSort[1]){
+        ages_InsSort[2] = ages_InsSort[1];
+        if (ages[3]<ages_InsSort[0]){
+            ages_InsSort[1] = ages_InsSort[0]
+            ages_InsSort[0] = ages[3];
+        }
+        else{
+            ages_InsSort[1] = ages[3];
+        }
+    }
+    else {
+        ages_InsSort[2] = ages[3];
+    }
+}
+else{
+    ages_InsSort[3] = ages[3]
+}
+
+//Now consider ages[4].
+if (ages[4]<ages_InsSort[3]){
+    InsSort[4] = InsSort[3];
+    if(ages[4]<ages_InsSort[2]){
+        ages_InsSort[3] = ages_InsSort[2];
+        if (ages[4]<ages_InsSort[1]){
+            ages_InsSort[2] = ages_InsSort[1];
+            if (ages[4]<ages_InsSort[0]){
+                ages_InsSort[1] = ages_InsSort[0];
+            }
+            else{
+                ages_InsSort[1] = ages[4];
+            }
+        }
+        else{
+         ages_InsSort[2] = ages[4];   I
+        }
+    }
+    else{
+        ages_InsSort[3] = ages[4];
+    }
+}
+else{
+    ages_InsSort[4] = ages[4];
+}
+
 
 return 0;
 }
+
 
 
 // 2. Quicksort function
