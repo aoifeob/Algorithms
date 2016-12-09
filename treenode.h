57ee7df6ad29657d2c3cdfecacd0d686f6a98228
obriea21 <obriea21@tcd.ie>
2016-12-09 18:21:56 +0000

    Add files via upload

---
+++ treenode.h
@@ -0,0 +1,17 @@
+//Aoife O' Brien
+//29th November 2016
+
+//start of header guard
+#ifndef TREENODE_H
+#define TREENODE_H
+
+//create structure to represent a node
+struct Tree_Node{
+char data; //data record
+Tree_Node* left; //pointer to left child
+Tree_Node* right; //pointer to right child
+};
+
+
+//end of header guard
+#endif
