d8cdde1a1b32fde3ca17ef50d394db7f83cb5249
obriea21 <obriea21@tcd.ie>
2016-12-11 20:43:23 +0000

    Create graph.hpp
    Adjacency list implementation of graph representation

---
+++ graph.hpp
@@ -0,0 +1,25 @@
+//start of header guard
+#ifndef GRAPH_H
+#define GRAPH_H
+
+//create node structure
+struct Node{
+    char data; //node name eg A,B,C
+    int weight; //weight of edge
+    struct Node* next; //pointer to next node in list
+};
+
+//create list structure
+struct List{
+    struct Node *start; //pointer to start point of list
+};
+
+//create graph structure
+struct Graph{
+        int Vertex; //number of vertices in the graph
+        struct List* array; //array of lists to make the graph
+};
+
+//end of header guard
+#endif
+
