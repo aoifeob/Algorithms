32cf166d796bc3012553cdd3f43aa523c58af70c
obriea21 <obriea21@tcd.ie>
2016-12-12 18:32:16 +0000

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -41,23 +41,46 @@ void newEdge(struct Graph* graph, int source, int data, int weight){
     graph->array[data].start = newNode1;
 }
 
+bool dfs(Graph* graph, char start_at, char find_value){
+    for (int i=0;i<12;i++){
+           
+    }
+          
+        if (start_at==find_value){
+            return 1;
+        }
+       else{
+            if ("start at hasn't been visited"){   
+                //mark it visited
+                for ("all nodes connected to it"){
+                    if ("specific node hasn't been visited"){
+                        dfs(mygraph, specificNode, find_value);//visit it   
+                    }
+                }
+            }
+       }
+}
+
 int main(){
     int Vertex=12;
-    struct Graph* graph = makeGraph(Vertex);
-    newEdge(graph,0,1,1);
-    newEdge(graph,1,2,2);
-    newEdge(graph,1,3,2);
-    newEdge(graph,2,4,3);
-    newEdge(graph,2,5,2);
-    newEdge(graph,3,6,2);
-    newEdge(graph,5,7,1);
-    newEdge(graph,5,9,4);
-    newEdge(graph,6,9,1);
-    newEdge(graph,6,11,3);
-    newEdge(graph,6,10,2);
-    newEdge(graph,9,8,2);
-    newEdge(graph,9,11,5);
-    newEdge(graph,8,10,11);
+    struct Graph* mygraph = makeGraph(Vertex);
+    newEdge(mygraph,0,1,1);
+    newEdge(mygraph,1,2,2);
+    newEdge(mygraph,1,3,2);
+    newEdge(mygraph,2,4,3);
+    newEdge(mygraph,2,5,2);
+    newEdge(mygraph,3,6,2);
+    newEdge(mygraph,5,7,1);
+    newEdge(mygraph,5,9,4);
+    newEdge(mygraph,6,9,1);
+    newEdge(mygraph,6,11,3);
+    newEdge(mygraph,6,10,2);
+    newEdge(mygraph,9,8,2);
+    newEdge(mygraph,9,11,5);
+    newEdge(mygraph,8,10,11);
+    
+    dfs(mygraph, 0, 23);
+    dfs(mygraph, 10, 1)
     
     free(Graph);
     free(List);
